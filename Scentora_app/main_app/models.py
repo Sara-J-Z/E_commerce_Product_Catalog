4272403c616ab7a57199b57818884425ff31884d
sara <sara@gmail.com>
2025-07-28 19:38:28 +0300

    Product Model

--- Scentora_app/main_app/models.py
+++ Scentora_app/main_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+# # Create your models here.
+# class Product(models.Model):
+#     name = models.CharField(max_length=255)
+#     description = models.TextField()
+#     # brand = models.ForeignKey('Brand', on_delete=models.CASCADE)
+#     # category = models.ForeignKey('Category', on_delete=models.CASCADE)
+#     price = models.DecimalField(max_digits=10, decimal_places=2)
+#     image = models.ImageField(upload_to='products/')
+#     is_active = models.BooleanField(default=True)
+#     created_at = models.DateTimeField(auto_now_add=True)
+
+#     def __str__(self):
+#         return self.name
