40b9b1f6d5a617af2f32fba6e38dba33406babd3
sara <sara@gmail.com>
2025-07-30 21:20:31 +0300

    fix merging2

--- Scentora_app/Scentora_app/settings.py
+++ Scentora_app/Scentora_app/settings.py
@@ -128,21 +128,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-<<<<<<< HEAD
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
-=======
-# Media files (for file uploads)
-
 import os
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
-import os
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-=========
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
->>>>>>>>> Temporary merge branch 2
